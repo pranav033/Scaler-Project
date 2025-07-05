395d03fc719b332d05e7209564c03db883639486
pranav033 <pranjalpranav0708@gmail.com>
2025-07-05 23:08:41 +0530

    added db dependencies

--- src/main/java/org/example/productcatalogservice/models/Product.java
+++ src/main/java/org/example/productcatalogservice/models/Product.java
@@ -1,17 +1,22 @@
 package org.example.productcatalogservice.models;
 
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel{
 
     private String name;
     private String description;
     private Double price;
     private String imageUrl;
+    @ManyToOne(cascade = CascadeType.ALL)
     private Category category;
     private Boolean isPrimeSaleSpecific;
 
