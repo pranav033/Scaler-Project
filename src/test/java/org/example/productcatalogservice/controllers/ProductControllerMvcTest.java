08ebc9f3dea95d435ccd90a85f130c105aa20465
pranav033 <pranjalpranav0708@gmail.com>
2025-08-02 21:49:27 +0530

    added mvc test and checked fetch types

--- src/test/java/org/example/productcatalogservice/controllers/ProductControllerMvcTest.java
+++ src/test/java/org/example/productcatalogservice/controllers/ProductControllerMvcTest.java
@@ -1,5 +1,6 @@
 package org.example.productcatalogservice.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.productcatalogservice.dtos.ProductDto;
 import org.example.productcatalogservice.models.Product;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -17,9 +19,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductController.class)
@@ -55,6 +59,24 @@ public class ProductControllerMvcTest {
         System.out.println(value);
 
         mockMvc.perform(get("/products"))
+                .andExpect(MockMvcResultMatchers.content().string(value))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testCreateProduct_RunSuccesfully() throws Exception {
+        Product pr1 = new Product();
+        pr1.setId(1l);
+        pr1.setName("S24");
+        when(iProductService.createProduct(any(Product.class))).thenReturn(pr1);
+        ProductDto prdto1 = new ProductDto();
+        prdto1.setId(1L);
+        prdto1.setName("S24");
+        String value = objectMapper.writeValueAsString(prdto1);
+
+        mockMvc.perform(post("/products")
+                        .content(value)
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.content().string(value))
                 .andExpect(status().isOk());
     }
--- src/test/java/org/example/productcatalogservice/repositories/CategoryRepoTest.java
+++ src/test/java/org/example/productcatalogservice/repositories/CategoryRepoTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,9 +22,24 @@ class CategoryRepoTest {
     @Transactional //Why I am adding this ?
     public  void testLoading() {
         Optional<Category> categoryOptional = categoryRepo.findById(2L);
-//        for(Product p : categoryOptional.get().getProducts()) {
-//            System.out.println(p.getName());
-//        }
+        for(Product p : categoryOptional.get().getProducts()) {
+            System.out.println(p.getName());
+        }
+    }
+
+
+    @Test
+    @Transactional
+    public void testForAllCategoriesForSubselect()
+    {
+        List<Category> categories = categoryRepo.findAll();
+        for(Category c : categories)
+        {
+            for(Product p : c.getProducts())
+            {
+                System.out.println(p.getName());
+            }
+        }
     }
 
 }
